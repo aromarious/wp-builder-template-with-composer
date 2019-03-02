1196eca0a9a39a7c917f2ca978f92d20f667c2d8
aromarious <6535448+aromarious@users.noreply.github.com>
2019-03-02 20:23:54 +0900

    Update builder environment generator

---
+++ make-builder-env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $1 = "--help" -o $# -ne 1 ]; then
+    echo Usage: $0 
+    echo After editing builder-settings.env, execute this command with no args.
+    exit
+fi
+source builder-settings.env
+
+cat template-composer.json \
+sed "s/%WP_CORE%/${WP_CORE}/" \
+sed "s/%BUILDER_NAME%/${BUILDER_NAME}" \
+sed "s/%SITENAME%/${SITENAME}/" \
+> composer.json
+
+cat template.gitignore \
+sed "s/%WP_CORE%/${WP_CORE}" \
+> .gitignore
+
+echo "COMPLETE: composer.json and .gitignore generated."
