c336f2e6f8832984229f6badf2bc63193624fd5b
aromarious <6535448+aromarious@users.noreply.github.com>
2019-03-02 23:36:00 +0900

    Added %BUILDER_EMAIL% etc

--- make-builder-env.sh
+++ make-builder-env.sh
@@ -7,13 +7,14 @@ fi
 source builder-settings.env
 
 cat template-composer.json \
-sed "s/%WP_CORE%/${WP_CORE}/" \
-sed "s/%BUILDER_NAME%/${BUILDER_NAME}" \
-sed "s/%SITENAME%/${SITENAME}/" \
+| sed "s/%WP_CORE%/${WP_CORE}/" \
+| sed "s/%BUILDER_NAME%/${BUILDER_NAME}/" \
+| sed "s/%BUILDER_EMAIL%/${BUILDER_EMAIL}/" \
+| sed "s/%SITENAME%/${SITENAME}/" \
 > composer.json
 
 cat template.gitignore \
-sed "s/%WP_CORE%/${WP_CORE}" \
+| sed "s/%WP_CORE%/${WP_CORE}/" \
 > .gitignore
 
 echo "COMPLETE: composer.json and .gitignore generated."
