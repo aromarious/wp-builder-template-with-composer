5511a1c330e1607d9af1baf68dffc189423dcd4e
aromarious <6535448+aromarious@users.noreply.github.com>
2019-03-17 15:24:22 +0900

    Changes to place wp-content directory out of wordpress core

--- make-builder-env.sh
+++ make-builder-env.sh
@@ -9,6 +9,7 @@ source builder-settings.env
 cat template-composer.json \
 | sed "s!%DOCROOT%!${DOCROOT}!" \
 | sed "s!%WP_CORE%!${WP_CORE}!" \
+| sed "s!%WP_CONTENT%!${WP_CONTENT}!" \
 | sed "s!%BUILDER_NAME%!${BUILDER_NAME}!" \
 | sed "s!%BUILDER_EMAIL%!${BUILDER_EMAIL}!" \
 | sed "s!%SITENAME%!${SITENAME}!" \
